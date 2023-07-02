45342996be7ac972c92b8b08351e1f23fe1f9e5a
teddy <teddywambua5456@gmail.com>
2023-07-03 00:54:49 +0300

    Completed assignment

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,20 @@ require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups)module MyApp
+class Application < Rails::Application
+  config.load_defaults 6.1
+  config.api_only = true
+
+  config.middleware.use ActionDispatch::Cookies
+  config.middleware.use ActionDispatch::Session::CookieStore
+
+  config.action_dispatch.cookies_same_site_protection = :strict
+end
+end
+
+
+
 
 module Phase4CookiesInRailsApi
   class Application < Rails::Application
